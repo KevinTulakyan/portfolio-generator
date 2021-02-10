c9a1e934964a3b1d43d7861fb56c2a44d9f63ca4
KevinTulakyan <KevinTulakyan@gmail.com>
2021-02-09 20:03:15 -0800

    generated profile

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const animalArray = ['dog', 'cat', 'pig'];
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-animalArray.push('cow');
+const profileDataArgs = process.argv.slice(2);
 
-const personObj = {
-  name: 'Lernantino',
-  age: 99
-};
+const [name, github] = profileDataArgs;
 
-personObj.age = 100;
-personObj.occupation = 'Developer';
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-  };
-  
-  printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
